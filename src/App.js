4ad539d49e19210c84ba83288362ac92989c60ef
jervalles <jer.vallles@outlook.com>
2019-11-21 22:44:16 +0100

    installed a new react app. installed NPM. cleaned the files.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import GameList from "./components/GameList.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <GameList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/GameList.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function GameList() {
+  return <div className="App">test</div>;
+}
+
+export default GameList;
