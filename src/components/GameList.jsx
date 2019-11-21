a8f3686c5d90df6e6dff0c6c119c03f0767a532a
jervalles <jer.vallles@outlook.com>
2019-11-21 23:13:24 +0100

    added axios and list of games is effective on GameList only

--- src/components/GameList.jsx
+++ src/components/GameList.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import axios from "axios";
 
-function GameList() {
-  return <div className="App">test</div>;
+class GameList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      games: []
+    };
+  }
+
+  componentDidMount() {
+    // Send the request
+    axios
+      .get("https://wild-games.herokuapp.com/api/v1")
+      // Extract the DATA from the received response
+      .then(({ data }) => {
+        console.log("Yay!");
+        const results = data;
+        console.log(results);
+        this.setState({
+          games: results
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div className="GameList">
+        test
+        {this.state.games.map(game => {
+          return (
+            <div>
+              <h4>{game.name}</h4>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default GameList;
