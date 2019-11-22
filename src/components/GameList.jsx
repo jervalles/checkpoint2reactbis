5759a71547b7f28d473f1f769e471e9a32bc6012
jervalles <jer.vallles@outlook.com>
2019-11-22 11:16:36 +0100

    GameList: Got now 2 functions for calling the full list or the filtered list. added the full list on the componentDidMount. and ternary added on the button

--- src/components/GameList.jsx
+++ src/components/GameList.jsx
@@ -1,35 +1,52 @@
 import React from "react";
-import axios from "axios";
+import Axios from "axios";
 import Game from "./Game.jsx";
 
 class GameList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      games: []
+      games: [],
+      changeButton: true
     };
   }
 
   componentDidMount() {
-    // Send the request
-    axios
-      .get("https://wild-games.herokuapp.com/api/v1")
-      // Extract the DATA from the received response
-      .then(({ data }) => {
-        console.log("Yay!");
-        const results = data;
-        console.log(results);
-        this.setState({
-          games: results
-        });
-      });
+    this.getGames();
+  }
+
+  getGames = () => {
+    Axios.get("https://wild-games.herokuapp.com/api/v1").then(response => {
+      const games = response.data;
+      this.setState({ games });
+    });
+  };
+
+  rateGames() {
+    const newGames = this.state.games.filter(game => game.rating >= 4.5);
+    this.setState({ games: newGames, changeButton: !this.state.changeButton });
+  }
+
+  displayAllGames() {
+    this.getGames();
+    this.setState({ changeButton: !this.state.changeButton });
   }
 
   render() {
     return (
       <div className="GameList">
+        <button
+          id="one"
+          onClick={
+            this.state.changeButton
+              ? toto => this.rateGames()
+              : roobert => this.displayAllGames()
+          }
+          className="gameListButton"
+        >
+          Higher Rating Games
+        </button>
         {this.state.games.map(game => {
-          console.log(game.name);
           return (
             <Game
               name={game.name}
