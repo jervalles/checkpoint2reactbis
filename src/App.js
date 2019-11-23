fd3f7c5f26a4933c0fd650cbe0c3bfa0456a4c75
jervalles <jer.vallles@outlook.com>
2019-11-23 13:39:29 +0100

    change Game function to Class. Added Props in App function

--- src/App.js
+++ src/App.js
@@ -2,34 +2,14 @@ import React from "react";
 import "./App.css";
 import GameList from "./components/GameList.jsx";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
-function App() {
+import GameScreens from "./components/GameScreens";
+function App(props) {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/my-profile">Profile</Link>
-            </li>
-            <li>
-              <Link to="/user-profile/Philaam">Philippe</Link>
-            </li>
-            <li>
-              <Link to="/user-profile/laurentmahieu">Laurent</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Switch>
-          <Route exact path="/">
-            <GameList />
-          </Route>
-        </Switch>
-      </div>
+      <Switch>
+        <Route exact path="/" component={GameList} />
+        <Route path="/game/:id" component={GameScreens} />
+      </Switch>
     </Router>
   );
 }
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,17 +1,40 @@
 import React from "react";
 import "../styles/Game.scss";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import GameScreens from "./GameScreens.jsx";
 
-function Game(props) {
-  return (
-    <div className="Game">
-      <h4>{props.name}</h4>
-      <div>
-        <img className="backgroundimages" src={props.background_image} />
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="Game">
+        <h4>{this.props.name}</h4>
+        <div>
+          <img className="backgroundimages" src={this.props.background_image} />
+        </div>
+        Rating : {this.props.rating}
+        <div>
+          <Router>
+            <div>
+              <nav>
+                <ul>
+                  <li>
+                    <Link to="/jeu/screenshots/">ScreenShots</Link>
+                  </li>
+                </ul>
+              </nav>
+
+              <Switch>
+                <Route path="/jeu/screenshots/:id" component={GameScreens} />
+              </Switch>
+            </div>
+          </Router>
+        </div>
       </div>
-      Rating : {props.rating}
-      <div></div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Game;
